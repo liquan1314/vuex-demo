db211b7d6562979c3ddb730dc586fa9d3d97eb7a
liquan1314 <lq1520299864@163.com>
2021-04-08 00:05:00 +0800

    这是一些vuex的基本操作的练习

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
   },
+  // 注意mutation里面的值不能是异步的，因为develop工具不会正常改变
   mutations: {
+    // mutation函数里面的第一个参数永远都是state
+    addCount(state) {
+      state.count++;
+    },
+    addCount1(state,step) {
+      state.count += step;
+    }
   },
+  // action 里面 是异步取到的值
   actions: {
+    // 注意action里面的第一个参数是context上下文的意思
+    asyncAdd(context) {
+      setTimeout(()=>{
+          context.commit("addCount");
+      },1000)
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-
+      <p>vuex state的值: {{$store.state.count}}</p>
+    <button @click="changeState">改变state的值</button>
+    <button @click="changeStateStep">增加固定的数值</button>
+    <button @click="asyncAdd">异步增加state的值</button>
   </div>
 </template>
 
@@ -9,5 +12,20 @@
 export default {
   name: 'Home',
 
+  methods: {
+    changeState() {
+      // commit是触发mutation的方法
+      this.$store.commit("addCount");
+    },
+
+    changeStateStep() {
+      this.$store.commit("addCount1",5);
+    },
+
+    asyncAdd () {
+      // 注意调用vuex里面的方法时需要调用dispatch
+      this.$store.dispatch("asyncAdd");
+    }
+  }
 }
 </script>
