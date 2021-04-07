5f55a97d1cd8f9b2b4f5f93b56df16e2f621a8c3
liquan1314 <lq1520299864@163.com>
2021-04-08 00:15:55 +0800

    增加另外一种取到store的值

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>vuex state的值:{{count}}</p>
+    <button @click="addCount">改变state的值</button>
+    <button @click="addCount1(5)">按照传参的值进行改变state的值</button>
+    <button @click="asyncAdd">异步改变state的值</button>
   </div>
 </template>
+<script>
+  // 这里面是state的和mutations 和actions的第二种取值方式 通过映射
+  import {mapState,mapMutations,mapActions} from "vuex";
+  export default {
+    name: "About",
+    computed: {
+      // 注意map这种映射传递的是一个数组
+      ...mapState(["count"]),
+    },
+
+    methods: {
+      ...mapMutations(["addCount","addCount1"]),
+      ...mapActions(["asyncAdd"])
+    }
+  }
+</script>
